1c7fb4e8d384ae933731b6bacc31b05091ec7ef5
topom21 <32741441+whq13018258357@users.noreply.github.com>
2023-02-09 09:51:15 +0800

    Add files via upload

---
+++ micn.sh
@@ -0,0 +1,102 @@
+pred_l=(96 192 336 720)
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode regre \
+        --data ETTm2 \
+        --features M \
+        --freq t \
+        --conv_kernel 13 25 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 96 \
+        --label_len 96 \
+        --pred_len $i
+done
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode regre \
+        --data ECL \
+        --features M \
+        --freq h \
+        --conv_kernel 13 25 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 96 \
+        --label_len 96 \
+        --pred_len $i
+done
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode regre \
+        --data Exchange \
+        --features M \
+        --freq d \
+        --conv_kernel 13 25 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 96 \
+        --label_len 96 \
+        --pred_len $i
+done
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode regre \
+        --data Traffic \
+        --features M \
+        --freq h \
+        --conv_kernel 13 25 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 96 \
+        --label_len 96 \
+        --pred_len $i
+done
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode regre \
+        --data WTH \
+        --features M \
+        --freq t \
+        --conv_kernel 13 25 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 96 \
+        --label_len 96 \
+        --pred_len $i
+done
+
+
+
+pred_l=(24 36 48 60)
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model micn \
+        --mode mean \
+        --data ILI \
+        --features M \
+        --freq d \
+        --conv_kernel 7 12 \
+        --d_layers 1 \
+        --d_model 512 \
+        --seq_len 36 \
+        --label_len 36 \
+        --pred_len $i
+done
+
---
+++ run.py
@@ -0,0 +1,134 @@
+import argparse
+import torch
+import numpy as np
+import random
+from exp.exp_informer import Exp_Informer
+
+
+
+parser = argparse.ArgumentParser(description='[MICN] Long Sequences Forecasting')
+
+parser.add_argument('--model', type=str, required=True, default='micn',help='model of experiment: MICN')
+
+parser.add_argument('--data', type=str, required=True, default='ETTh1', help='data')
+parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
+parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')    
+parser.add_argument('--features', type=str, default='M', help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+parser.add_argument('--freq', type=str, default='h', help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+parser.add_argument('--conv_kernel', type=int, nargs='+', default=[17,49], help='downsampling and upsampling convolution kernel_size')
+parser.add_argument('--decomp_kernel', type=int, nargs='+', default=[17,49], help='decomposition kernel_size')
+parser.add_argument('--isometric_kernel', type=int, nargs='+', default=[17,49], help='isometric convolution kernel_size')
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of Informer encoder')
+parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
+parser.add_argument('--pred_len', type=int, default=24, help='prediction sequence length')
+
+parser.add_argument('--mode', type=str, default='regre', help='different mode of trend prediction block: [regre or mean]')
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+parser.add_argument('--padding', type=int, default=0, help='padding type')
+parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+parser.add_argument('--embed', type=str, default='timeF', help='time features encoding, options:[timeF, fixed, learned]')
+parser.add_argument('--activation', type=str, default='gelu',help='activation')
+parser.add_argument('--output_attention',action='store_true', help='whether to output attention in ecoder')
+parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+parser.add_argument('--cols', type=str, nargs='+', help='certain cols from the data files as the input features')
+parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+
+parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test', help='exp description')
+parser.add_argument('--loss', type=str, default='mse', help='loss function')
+parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
+
+parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+parser.add_argument('--gpu', type=int, default=0, help='gpu')
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
+
+args = parser.parse_args()
+
+args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+if args.use_gpu and args.use_multi_gpu:
+    args.devices = args.devices.replace(' ','')
+    device_ids = args.devices.split(',')
+    args.device_ids = [int(id_) for id_ in device_ids]
+    args.gpu = args.device_ids[0]
+
+data_parser = {
+    'ETTh1':{'data':'ETTh1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
+    'ETTh2':{'data':'ETTh2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
+    'ETTm1':{'data':'ETTm1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
+    'ETTm2':{'data':'ETTm2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
+    'WTH':{'data':'weather.csv','T':'OT','M':[21,21,21],'S':[1,1,1],'MS':[21,21,1]},
+    'ECL':{'data':'electricity.csv','T':'OT','M':[321,321,321],'S':[1,1,1],'MS':[321,321,1]},
+    'Traffic': {'data': 'traffic.csv', 'T': 'OT', 'M': [862, 862, 862], 'S': [1, 1, 1], 'MS': [862, 862, 1]},
+    'Exchange': {'data': 'exchange_rate.csv', 'T': 'OT', 'M': [8, 8, 8], 'S': [1, 1, 1], 'MS': [8, 8, 1]},
+    'ILI': {'data': 'national_illness.csv', 'T': 'OT', 'M': [7, 7, 7], 'S': [1, 1, 1], 'MS': [7, 7, 1]},
+}
+if args.data in data_parser.keys():
+    data_info = data_parser[args.data]
+    args.data_path = data_info['data']
+    args.target = data_info['T']
+    args.enc_in, args.dec_in, args.c_out = data_info[args.features]
+args.detail_freq = args.freq
+args.freq = args.freq[-1:]
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+setup_seed(2021)
+
+decomp_kernel = []  # kernel of decomposition operation 
+isometric_kernel = []  # kernel of isometric convolution
+for ii in args.conv_kernel:
+    if ii%2 == 0:   # the kernel of decomposition operation must be odd
+        decomp_kernel.append(ii+1)
+        isometric_kernel.append((args.seq_len + args.pred_len+ii) // ii) 
+    else:
+        decomp_kernel.append(ii)
+        isometric_kernel.append((args.seq_len + args.pred_len+ii-1) // ii) 
+args.isometric_kernel = isometric_kernel  # kernel of isometric convolution
+args.decomp_kernel = decomp_kernel   # kernel of decomposition operation 
+  
+
+print('Args in experiment:')
+print(args)
+Exp = Exp_Informer
+for ii in range(args.itr):
+    # setting record of experiments
+    setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_eb{}_{}'.format(args.model, args.data, args.mode, args.features, 
+                args.seq_len, args.label_len, args.pred_len,
+                args.d_model, args.n_heads, args.e_layers, args.d_layers, args.d_ff, 
+                args.embed, ii)
+
+    exp = Exp(args)  # set experiments
+    print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+    exp.train(setting)
+
+    print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting)
+
+    if args.do_predict:
+        print('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+        exp.predict(setting, True)
+
+    torch.cuda.empty_cache()
